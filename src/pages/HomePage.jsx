44354fee1c9d4448d31b047f24cea7d50131c3db
DidasMba <daniel.mbarushi@gmail.com>
2024-03-16 10:25:57 +0300

    homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+// HomePage.js
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Welcome to My Portfolio</h2>
+      {/* Add more content here */}
+    </div>
+  );
+}
+
+export default HomePage;
