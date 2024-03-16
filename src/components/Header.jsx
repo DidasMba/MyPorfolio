10d0a71acecdceb9ce99fa76537aae2efd24eb7f
DidasMba <daniel.mbarushi@gmail.com>
2024-03-16 11:16:16 +0300

    header edit2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,7 @@ const Header = () => {
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/service">Service</a></li>
           <li><a href="/portfolio">Portfolio</a></li>
-          <li><a href="/resume">Resume</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
