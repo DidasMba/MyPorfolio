21b795c8978b7dda7d331097b6cb0bb82091bd6e
DidasMba <daniel.mbarushi@gmail.com>
2024-03-16 12:08:17 +0300

    ContactPage

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+// ContactPage.js
+
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      {/* Add contact form or details here */}
+    </div>
+  );
+}
+
+export default ContactPage;
