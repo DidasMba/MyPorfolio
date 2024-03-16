d256dfc150dbcfa9565c2cabb8eea2f809229b2e
DidasMba <daniel.mbarushi@gmail.com>
2024-03-16 12:06:42 +0300

    Portfoliopage

---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,14 @@
+// PortfolioPage.js
+
+import React from 'react';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      {/* Add portfolio content here */}
+    </div>
+  );
+}
+
+export default PortfolioPage;
